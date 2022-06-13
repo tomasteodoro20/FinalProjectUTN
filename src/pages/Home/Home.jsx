5e2b87fc9db671fa95a1976008b1e38be5697bb9
tomasteodoro20 <tomaschomyszyn@gmail.com>
2022-06-13 14:11:20 -0300

    Se agrego el "Header"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, Headers} from "react";
 import { Navbar } from "react-bootstrap";
 import "../../components/carousel/Controlled-Carousel"
 import ControlledCarousel from "../../components/carousel/Controlled-Carousel";
@@ -8,7 +8,9 @@ import "./home.css"
 function Home() {
     return (
     <>
+    <Headers>
     <Navbar/>
+    </Headers>
     <ControlledCarousel/>
     </>
     )
