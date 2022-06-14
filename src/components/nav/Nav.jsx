657937a5e13e6014deaf55b97fdf30b404684eeb
MelaniVila <melanib.vila@gmail.com>
2022-06-14 11:23:54 -0300

    Footer listo

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,6 +6,15 @@ import './nav.css'
 function NavFooter() {
   return (
   <>
+  <footer className="footer-container">
+    <span className='text-color'>2022 - ©Chomyszyn/Vila - Powered by <a href="https://rawg.io/">RAWG.io</a></span>  
+    <div className="redes-sociales">
+    <a href="https://facebook.com/"><img height="20" src='icons-facebook.png' alt="facebook" /></a>
+    <a href="https://instagram.com/"><img height="20" src='icons-instagram.png ' alt="instagram" /></a>
+    <a href="https://twitter.com/"><img height="20" src='icons-twitter.png' alt="twitter" /></a>
+    </div>
+  </footer>
+{/* 
   <Nav className="justify-content-end bg-color" activeKey="/home">  
   <p className='text-color'>2022 - ©Chomyszyn/Vila - Powered by RAWG.io</p>
     <Nav.Item>
@@ -17,7 +26,7 @@ function NavFooter() {
     <Nav.Item>
       <Nav.Link eventKey="link-2"><img height="20" src='icons-twitter.png' alt="twitter" /></Nav.Link>
     </Nav.Item>
-  </Nav>
+  </Nav> */}
   </>
   )
 }
