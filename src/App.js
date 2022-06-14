2823d1a9a4e9b934c2c5bf53a45fda3a548fd666
tomasteodoro20 <tomaschomyszyn@gmail.com>
2022-06-14 09:47:59 -0300

    Se inserto la "page" "Home"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import NavBar from './components/navbar/NavBar';
-import ControlledCarousel from './components/carousel/Controlled-Carousel';
+import Home from './pages/Home/Home'
+// import NavBar from './components/navbar/NavBar';
+// import ControlledCarousel from './components/carousel/Controlled-Carousel';
+// import NavFooter from './components/nav/Nav';
 
 function App() {
   return (
     <>
-    <NavBar />
-    <ControlledCarousel/>
+    <Home/>
     </>
   );
 }
