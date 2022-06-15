5a148eb9342ac9fa73e6bbc3f74d6bac0848625b
tomasteodoro20 <tomaschomyszyn@gmail.com>
2022-06-15 14:59:02 -0300

    Se creo el complemento "divider"

---
+++ src/components/divider/Divider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './divider.css';
+
+function Divider () {
+    return (
+        <>
+        <hr className='divider'/>
+        </>
+    )
+}
+
+export default Divider;
