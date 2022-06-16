a4141dc0a76c75fd5080d346e547ae7080548b5f
tomasteodoro20 <tomaschomyszyn@gmail.com>
2022-06-16 16:11:35 -0300

    Genere la pagina Mi Perfil

---
+++ src/pages/Profile/MyProfile.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Button, Image, Container, Col, Row} from 'react-bootstrap';
+import NavBar from '../../components/navbar/NavBar';
+import NavFooter from '../../components/nav/NavFooter';
+import Divider from '../../components/divider/Divider';
+import './profile.css'
+import WishlistButton from '../../components/wishlist/WishlistButton';
+
+function MyProfile () {
+
+    return (
+    <>
+    <NavBar/>
+    <Container>
+    <h2 className='title-color'>Mi Perfil</h2>
+    <Button className='delete-profile-btn'>Eliminar cuenta</Button>
+    <Divider/>
+    </Container>
+        <Row>
+        <Col>
+        <Image src='logo.png' roundedCircle></Image>
+        </Col>
+        <Col className='text-color'>
+        <p>Usuario: </p>
+        <p>Nombre: </p> 
+        <p>Apellido: </p>
+        <p>E-mail: </p>
+        <p>Fecha de nacimiento: </p>
+    </Col>
+    <Col>
+    <WishlistButton/>
+    </Col>
+    </Row>
+    <Divider/>
+    <NavFooter/>
+    </>
+    )
+}
+
+export default MyProfile;
