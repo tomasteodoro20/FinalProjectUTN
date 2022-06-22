b09c31f8613df3881f23bfe5521ef8eedee677e1
MelaniVila <melanib.vila@gmail.com>
2022-06-22 13:24:19 -0300

    update api constants y ficha game

--- src/components/game-data/GameData.jsx
+++ src/components/game-data/GameData.jsx
@@ -1,36 +1,55 @@
 import React, {useState, useEffect} from "react";
 import './game-data.css';
 import GameScreenshots from "./GameScreenshots";
+import Divider from "../divider/Divider";
+import { FaWindows, FaPlaystation, FaXbox } from "react-icons/fa";
+import { SiNintendoswitch } from "react-icons/si";
+
 
 function GameData () {
+    
     const [gameDetails, setGameDetails] = useState([]);
     const fetchDetails = async () => {
-    const response = await fetch(
-        "https://api.rawg.io/api/games/123?key=55381525b8814ed8b7afabaa2999118a"
-      );
-     const data = await response.json();
-     setGameDetails(data);
-    };
-  
-    useEffect(() => {
-        fetchDetails();
-    }, []);
+        const response = await fetch(
+            "https://api.rawg.io/api/games/123?key=55381525b8814ed8b7afabaa2999118a"
+            );
+            const data = await response.json();
+            setGameDetails(data);
+        };
+        
+        useEffect(() => {
+            fetchDetails();
+        }, []);
+        
+        const ratingStyle = `${gameDetails.rating > 3.5 ? "good" : "bad"}-rating`;
 
     return (
+    <>
+    <div className="game-title">
+            <h2>{gameDetails.name}</h2>            
+            <span className={ratingStyle}>Rating: {gameDetails.rating}</span>
+    </div>
+    <Divider />
     <div className="game-data">
         <div className="game-details">
-            <h2>{gameDetails.name}</h2>
-            <p>Rating: {gameDetails.rating}</p>
             <p
                 dangerouslySetInnerHTML={{__html: gameDetails.description}}
             />         
             <p>Fecha de lanzamiento: {gameDetails.released}</p>
-            <a href={gameDetails.website}>Sitio oficial</a>
+            <div className="available-consoles">
+                <FaWindows/>
+                <FaPlaystation/>
+                <FaXbox/>
+                <SiNintendoswitch />
+            </div>
+            <p>Disponible en las siguientes plataformas: </p>
+            <p className="website-button"><a href={gameDetails.website}>Sitio oficial</a></p>
         </div>
         <div className="game-screenshots">
             <GameScreenshots/>        
         </div>
     </div>
+    </>
     );
   }
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -26,7 +26,7 @@ const currentMonth = getCurrentMonth();
 const currentDay = getCurrentDay();
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
-// const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // //Popular games
 // const popular_games = `games?key=${apiKey}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
@@ -84,6 +84,7 @@ export const samplePublisherGamesGet = publisher_id => `${ base_url }${ publishe
 
 export const popularGames = `${base_url}games?key=${apiKey}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=20`
 export const latestGames = `${base_url}games?key=${apiKey}&dates=${lastYear},${currentDate}&ordering=-released&page_size=4`
+export const upcomingGames = `games?key=${apiKey}&dates=${currentDate},${nextYear}&ordering=-added&page_size=20`
 
 export const fetchActionGames = `${base_url}games?key=${apiKey}&genres=4`;
 export const fetchAdventureGames = `${base_url}games?key=${apiKey}&genres=3`;
