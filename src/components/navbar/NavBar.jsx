5b7e051c64c28668f568b2444618543220303251
MelaniVila <melanib.vila@gmail.com>
2022-06-23 10:47:21 -0300

    href dropdowns

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -15,26 +15,26 @@ function NavBar() {
         <Navbar.Collapse id="navbarScroll">
         <Nav className="me-auto my-2 my-lg-0" navbarScroll>
             <NavDropdown title="GÉNEROS" id="navbarScrollingDropdown" className="fontSize">            
-                <NavDropdown.Item href="#action4"><Link className="link-style" to='/Action'>Acción</Link></NavDropdown.Item>
+                <NavDropdown.Item href="/Action"><Link className="link-style" to='/Action'>Acción</Link></NavDropdown.Item>
                 <NavDropdown.Divider />   
-                <NavDropdown.Item href="#action4"><Link className="link-style" to='/Adventure'>Aventura</Link></NavDropdown.Item>
+                <NavDropdown.Item href="/Adventure"><Link className="link-style" to='/Adventure'>Aventura</Link></NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action4"><Link className="link-style" to='/Strategy'>Estrategia</Link></NavDropdown.Item>
+                <NavDropdown.Item href="/Strategy"><Link className="link-style" to='/Strategy'>Estrategia</Link></NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action4"><Link className="link-style" to='/Racing'>Carrera</Link></NavDropdown.Item>
+                <NavDropdown.Item href="/Racing"><Link className="link-style" to='/Racing'>Carrera</Link></NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action4"><Link className="link-style" to='/Shooter'>Shooter</Link></NavDropdown.Item>
+                <NavDropdown.Item href="/Shooter"><Link className="link-style" to='/Shooter'>Shooter</Link></NavDropdown.Item>
                 <NavDropdown.Divider />         
-                <NavDropdown.Item href="#action4"><Link className="link-style" to='/Simulation'>Simulación</Link></NavDropdown.Item>
+                <NavDropdown.Item href="/Simulation"><Link className="link-style" to='/Simulation'>Simulación</Link></NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="CONSOLAS" id="navbarScrollingDropdown" className="fontSize">            
-                <NavDropdown.Item href="#action4"><Link className="link-style" to='/PC'>PC</Link></NavDropdown.Item>
+                <NavDropdown.Item href="/PC"><Link className="link-style" to='/PC'>PC</Link></NavDropdown.Item>
                 <NavDropdown.Divider />   
-                <NavDropdown.Item href="#action4"><Link className="link-style" to='/PlayStation'>PlayStation</Link></NavDropdown.Item>
+                <NavDropdown.Item href="/PlayStation"><Link className="link-style" to='/PlayStation'>PlayStation</Link></NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action4"><Link className="link-style" to='/Xbox'>Xbox</Link></NavDropdown.Item>
+                <NavDropdown.Item href="/Xbox"><Link className="link-style" to='/Xbox'>Xbox</Link></NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action4"><Link className="link-style" to='/Nintendo'>Nintendo</Link></NavDropdown.Item>
+                <NavDropdown.Item href="/Nintendo"><Link className="link-style" to='/Nintendo'>Nintendo</Link></NavDropdown.Item>
             </NavDropdown>
             <Button className="upcoming-btn"><Link className="upcoming-btn-style" to='/Upcoming'>Proximamente</Link></Button>
         </Nav>
