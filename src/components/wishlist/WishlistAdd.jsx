7cc6dae6e628ac84f7c59e9a21b03c0f7ce5acb9
tomasteodoro20 <tomaschomyszyn@gmail.com>
2022-06-24 12:12:47 -0300

    Boton add wishlist

---
+++ src/components/wishlist/WishlistAdd.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './wishlist-button.css';
+
+
+function WishlistBtnAdd () {
+    
+    return (
+<>
+        <button className='custom-btn' type="button">
+            Añadir a la Lista de Deseados ❤
+        </button>
+</>
+ )
+}
+
+export default WishlistBtnAdd;
--- src/components/wishlist/WishlistButton.jsx
+++ src/components/wishlist/WishlistButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-;import './wishlist-button.css'
+import { Link } from 'react-router-dom';
+import './wishlist-button.css';
 
 
 function WishlistBtn () {
