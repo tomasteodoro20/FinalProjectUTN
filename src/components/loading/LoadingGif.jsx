271d499cbda987554e2749c900df02f75ad5f2f8
tomasteodoro20 <tomaschomyszyn@gmail.com>
2022-06-29 17:09:55 -0300

    Se agrego el loading gif

--- src/components/loading/LoadingGif.jsx
+++ src/components/loading/LoadingGif.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function LoadingGif () { 
-    const loadingGif = require("loading-gif.gif")
+    const loadingGif = require("./loading-gif.gif")
     return (
     <>
         <img src={loadingGif} alt="Cargando..."/>
