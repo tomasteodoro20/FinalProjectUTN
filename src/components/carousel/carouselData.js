ae198acf99dbd7a987fc6745d757708a0f7e5438
tomasteodoro20 <tomaschomyszyn@gmail.com>
2022-06-29 11:47:25 -0300

    Empece el loading

--- src/components/carousel/carouselData.js
+++ src/components/carousel/carouselData.js
@@ -2,21 +2,21 @@ const videoProperties = [
     {
       id:1,
       title: "The Last of Us",
-      src: 'https://www.youtube.com/watch?v=9pmw4z3k_zg',
+      src: 'http://www.youtube.com/watch?v=9pmw4z3k_zg',
       slug:`/the-last-of-us-part-i`,
       description:"Revisit the game that set a new bar for single-player narrative storytelling with The Last of Us™ and explore a ravaged and hardened world, where every action has a brutal consequence for Joel and Ellie."
     },
     {
       id:2,
       title: "Star Wars Jedi Fallen Order",
-      src: 'https://www.youtube.com/watch?v=xIl2z5wwjdA',
+      src: 'http://www.youtube.com/watch?v=xIl2z5wwjdA',
       slug:`/jedi-the-fallen-order`,
       description:"Star Wars Jedi: Fallen Order is an action-adventure game set after Star Wars: Episode III — Revenge of the Sith™. Develop your Force abilities, hone your lightsaber techniques, and explore the ancient mysteries of a long-lost civilization—all while staying one step ahead of the Empire and its deadly Inquisitors."
     },
     {
       id:3,
       title: "Ghost of Tsushima",
-      src: 'https://www.youtube.com/watch?v=jPeyWo1fWRw',
+      src: 'http://www.youtube.com/watch?v=jPeyWo1fWRw',
       slug:`/ghost-of-tsushima`,
       description:"The year is 1274. Samurai warriors are the legendary defenders of Japan--until the fearsome Mongol Empire invades the island of Tsushima, wreaking havoc and conquering the local population. As one of the last surviving samurai, you rise from the ashes to fight back. But, honorable tactics won't lead you to victory."
     }
