284702fb47fab339516e625c9a6e5a403a6de4b9
MelaniVila <melanib.vila@gmail.com>
2022-07-12 15:05:49 -0300

    register form working

--- src/components/register-form/RegisterForm.jsx
+++ src/components/register-form/RegisterForm.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Formik, Form, Field, useFormik } from "formik";
 import * as yup from 'yup';
 import axios from 'axios';
-import { database } from "../../db-config/db";
+// import { database } from "../../db-config/db";
 import { useState } from "react";
 import "./register-form.css";
 
 const RegisterForm = () => {
+    const [success, setSuccess] = useState(null);
+    const [error, setError ] = useState(null);
     const {values, setValues} = useState({
         "username":"",
         "password":"",
@@ -24,8 +26,10 @@ const validationSchema = yup.object({
         password: yup.string()
             .min(5, "No puede tener menos de 5 caracteres")
             .required("Introduzca una contraseña por favor"),
-        repeatpassword: yup.string()
-            .oneOf([yup.ref('password'), null], 'Las contraseñas deben ser iguales'),
+        confirmPassword: yup.string().when("password", {
+            is: val => (val && val.length > 0 ? true : false),
+            then: yup.string().oneOf([yup.ref("password")], "Las contraseñas deben ser iguales")
+            }),
         firstname: yup.string()
             .min(2, "No puede contener menos de 2 caracteres")
             .max(15, "No puede tener mas de 15 caracteres")
@@ -39,17 +43,36 @@ const validationSchema = yup.object({
             .required("Introduzca un mail por favor"),
     })
     
-const url = database;
+// const url = database;
 
 const onChange = async (e) => {    
     await setValues(values[e.target.name] = e.target.value)
 }
 
-const onSubmit = (values, {setValues}) => {        
-    setValues(values);    
-    axios.post(url, values)
-    // alert(JSON.stringify(values));
-};
+const onSubmit = async (values) => {
+    const {confirmPassword, ...data} = values;
+
+    const response = await axios.post("http://localhost:5000/register", data).catch((err) => {
+      if(err && err.response)
+        // console.log("Error" + data)
+        setError(err.response.data.message)
+        setSuccess(null);
+    })
+    
+    if (response && response.data) {
+    //   console.log("Funciona" + data)
+      setError(null);
+      setSuccess(response.data.message);
+      formik.resetForm();
+    }
+    
+  };
+
+// const onSubmit = (values, {setValues}) => {        
+//     setValues(values);    
+//     axios.post(url, values)
+//     // alert(JSON.stringify(values));
+// };
 
 // const onSubmit = (values, { setSubmitting }) => {
 //     setTimeout(() => {
@@ -123,7 +146,9 @@ const formik = useFormik({
         //         }, 500);
         //     }}        
         >
-            <Form className="register-form" onSubmit={formik.handleSubmit}>
+        <Form className="register-form" onSubmit={formik.handleSubmit}>
+        {!error && <span style={{color:"green"}}>{success ? success : ""}</span>}
+        {!success && <span style={{color:"red"}}>{error ? error : ""}</span>}
             <label htmlFor="username">Nombre de usuario</label>
             <Field name="username" type="text"  
             value={formik.values.username || ""} 
@@ -142,14 +167,14 @@ const formik = useFormik({
             {formik.touched.password && formik.errors.password ? 
             <span className="error-color">{formik.errors.password}</span> : ""}
             
-            <label htmlFor="repeatpassword">Confirmar Contraseña</label>
-            <Field name="repeatpassword" type="password"
-            value={formik.values.repeatpassword || ""} 
+            <label htmlFor="confirmPassword">Confirmar Contraseña</label>
+            <Field name="confirmPassword" type="password"
+            value={formik.values.confirmPassword || ""} 
             onChange={formik.handleChange} 
             onBlur={formik.handleBlur}
             />
-            {formik.touched.repeatpassword && formik.errors.repeatpassword ? 
-            <span className="error-color">{formik.errors.repeatpassword}</span> : ""}
+            {formik.touched.confirmPassword && formik.errors.confirmPassword ? 
+            <span className="error-color">{formik.errors.confirmPassword}</span> : ""}
             
             <label htmlFor="firstname">Nombre</label>
             <Field name="firstname" type="text"
