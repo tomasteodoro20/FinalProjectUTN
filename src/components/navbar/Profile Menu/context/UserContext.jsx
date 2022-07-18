58b43995d44ffb169864fcd14634e24003da2c05
tomasteodoro20 <tomaschomyszyn@gmail.com>
2022-07-18 16:14:58 -0300

    Logout backend

--- src/components/navbar/Profile Menu/context/UserContext.jsx
+++ src/components/navbar/Profile Menu/context/UserContext.jsx
@@ -3,7 +3,9 @@ import React, {useState} from "react";
 const Context = React.createContext({})
 
 export function UserContextProvider ({children}) {
-    const [password, setPassword] = useState(null)
+    const [password, setPassword] = useState(
+        () => window.sessionStorage.getItem('password')
+        )
 
     return <Context.Provider value={{password, setPassword}}>
         {children}
